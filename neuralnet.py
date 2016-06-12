6c5384b6b5a1f61bd4635178386e2bfa3b1fcd59
Antti Juvonen <akajuvonen@users.noreply.github.com>
2016-06-12 15:23:01 +0300

    Added sigmoid function (or derivative)

--- neuralnet.py
+++ neuralnet.py
@@ -1,8 +1,23 @@
 #!/usr/bin/env python
 
+from numpy import exp
+
 class NeuralNet():
     def __init__(self):
         pass
+
+    def _sigmoid(self,x,deriv):
+        """The sigmoid function (or it's derivative).
+        Arguments:
+        x -- The weighted sum of an input
+        deriv -- Boolean, if True, we return the sigmoid derivative
+        Returns:
+        The sigmoid function value, or the derivative if needed.
+        """
+        if deriv:
+            return x*(1-x)
+        else:
+            return 1/(1+exp(-x))
 
     def train(self):
         pass
