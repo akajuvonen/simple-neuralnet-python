5850f2e71098844aa0e94c95d9684f8a0183505d
Antti Juvonen <akajuvonen@users.noreply.github.com>
2016-06-22 12:53:02 +0300

    Added tests for sigmoid function

--- tests/test_neuralnet.py
+++ tests/test_neuralnet.py
@@ -5,3 +5,18 @@ class NeuralNetTest(unittest.TestCase):
     def setUp(self):
         """Setup method for neural net tests"""
         self.net = NeuralNet()
+
+    def testSigmoid(self):
+        """Sigmoid method tests"""
+        # Sigmoid function value should be close to 0
+        i=-10
+        result = self.net._sigmoid(i)
+        self.assertTrue(result<0.1)
+        # Should be 0.5
+        i=0
+        result = self.net._sigmoid(i)
+        self.assertEquals(result,0.5)
+        # Should be close to 1
+        i=10
+        result = self.net._sigmoid(i)
+        self.assertTrue(result>0.9)
