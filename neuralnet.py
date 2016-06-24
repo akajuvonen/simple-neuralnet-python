158b8ae99848bb71eaca9ce2eb95ea8e94b00179
Antti Juvonen <akajuvonen@users.noreply.github.com>
2016-06-24 12:24:26 +0300

    Added clarification to sigmoid deriv

--- neuralnet.py
+++ neuralnet.py
@@ -16,6 +16,15 @@ class NeuralNet():
         return 1/(1+exp(-x))
 
     def _sigmoid_deriv(self,y):
+        """The sigmoid derivative function.
+        Arguments:
+        y -- The neural network outputs. Notice that
+        these values have already been calculated using
+        the sigmoid function. An actual sigmoid derivative
+        should be sigmoid(x)*(1-sigmoid(x)).
+        Return:
+        The sigmoid derivative.
+        """
         return y*(1-y)
 
     def train(self):
