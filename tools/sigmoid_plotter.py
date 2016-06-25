08cb60f8f1a981edc5edaa2f226588eb5366316f
Antti Juvonen <akajuvonen@users.noreply.github.com>
2016-06-25 19:56:07 +0300

    Added comments to sigmoid plotter

--- tools/sigmoid_plotter.py
+++ tools/sigmoid_plotter.py
@@ -3,9 +3,13 @@
 from neuralnet import NeuralNet
 import matplotlib.pyplot as plt
 
+# Plots sigmoid function and it's derivative using matplotlib
+
 def main():
+    # Create the net that includes sigmoid functions
     net = NeuralNet()
 
+    # Populate lists with sigmoid values
     sigmoid_values = []
     for i in range(-10,11):
         sigmoid_values.append(net._sigmoid(i))
@@ -13,11 +17,13 @@ def main():
     for i in sigmoid_values:
         sigmoid_deriv_values.append(net._sigmoid_deriv(i))
 
+    # Plot and set some parameters for plt
     plt.plot(range(-10,11),sigmoid_values)
     plt.plot(range(-10,11),sigmoid_deriv_values)
     plt.ylabel('Value')
     plt.title('Sigmoid and its derivative')
     plt.grid(True)
+    # Save fig and show
     plt.savefig('sigmoid.png')
     plt.show()
 
