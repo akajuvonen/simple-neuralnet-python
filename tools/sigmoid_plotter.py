6b1a12c4a7a9e363a36364a286e18abb426ca91e
Antti Juvonen <akajuvonen@users.noreply.github.com>
2016-06-25 19:41:59 +0300

    Calculated sigmoid and derivative values and added to lists

--- tools/sigmoid_plotter.py
+++ tools/sigmoid_plotter.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python
 
+from neuralnet import NeuralNet
+
 def main():
-    pass
+    net = NeuralNet()
+    sigmoid_values = []
+    for i in range(-10,11):
+        sigmoid_values.append(net._sigmoid(i))
+    sigmoid_deriv_values = []
+    for i in sigmoid_values:
+        sigmoid_deriv_values.append(net._sigmoid_deriv(i))
+    print(sigmoid_values)
+    print(sigmoid_deriv_values)
 
 if __name__=='__main__':
     main()
