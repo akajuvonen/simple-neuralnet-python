e22103b5d6a81c6e97912c09088ecbb51e93e1ef
Antti Juvonen <akajuvonen@users.noreply.github.com>
2016-06-25 19:48:17 +0300

    Added sigmoid deriv plot, also title and ylabel, grid

--- tools/sigmoid_plotter.py
+++ tools/sigmoid_plotter.py
@@ -5,13 +5,20 @@ import matplotlib.pyplot as plt
 
 def main():
     net = NeuralNet()
+
     sigmoid_values = []
     for i in range(-10,11):
         sigmoid_values.append(net._sigmoid(i))
     sigmoid_deriv_values = []
     for i in sigmoid_values:
         sigmoid_deriv_values.append(net._sigmoid_deriv(i))
+
     plt.plot(range(-10,11),sigmoid_values)
+    plt.plot(range(-10,11),sigmoid_deriv_values)
+    plt.ylabel('Value')
+    plt.title('Sigmoid and its derivative')
+    plt.grid(True)
+    plt.savefig('sigmoid.png')
     plt.show()
 
 if __name__=='__main__':
