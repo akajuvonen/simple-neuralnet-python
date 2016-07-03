601b1da05c3eaa7bb289921f21900342bf2e8a71
Antti Juvonen <akajuvonen@users.noreply.github.com>
2016-07-03 12:47:59 +0300

    Added layer class

--- layer.py
+++ layer.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
 
+from numpy.random import random
+
 class Layer():
-    def __init__(self):
-        pass
+    def __init__(self,neurons,inputs):
+        self.weights = random((inputs,neurons))
 
 def main():
-    pass
+    layer = Layer(5,3)
+    print(layer.weights)
 
 if __name__=='__main__':
     main()
