3063aca28bd9bfa5f4cb762a8951bf6e136e92c0
Antti Juvonen <akajuvonen@users.noreply.github.com>
2016-07-03 12:50:45 +0300

    Added comments to layer

--- layer.py
+++ layer.py
@@ -3,10 +3,17 @@
 from numpy.random import random
 
 class Layer():
+    """A neural network layer class that has it's own weights."""
     def __init__(self,neurons,inputs):
+        """Init for layer.
+        Arguments:
+        neurons -- How many neurons in this layer (int)
+        inputs -- How many inputs per neuron (int)
+        """
         self.weights = random((inputs,neurons))
 
 def main():
+    # Create a layer and print its weights
     layer = Layer(5,3)
     print(layer.weights)
 
