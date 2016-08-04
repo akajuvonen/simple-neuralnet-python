f1e3b53575f5fd436e0d43aa5aca3e84c858cb40
Antti Juvonen <akajuvonen@users.noreply.github.com>
2016-08-04 16:18:53 +0300

    Added comments

--- tests/iris_test.py
+++ tests/iris_test.py
@@ -2,6 +2,10 @@
 
 from sklearn import datasets
 
+# This example loads the IRIS dataset and classifies
+# using our neural network implementation.
+# The results are visualized in a 2D-plot.
+
 def main():
     iris = datasets.load_iris()
     X = iris.data
