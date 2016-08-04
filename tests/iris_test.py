c49d5cf3b2d9e21c9c84280c9b0995d59c8dc632
Antti Juvonen <akajuvonen@users.noreply.github.com>
2016-08-04 16:17:56 +0300

    Completed iris data loading and target loading to variables

--- tests/iris_test.py
+++ tests/iris_test.py
@@ -3,7 +3,9 @@
 from sklearn import datasets
 
 def main():
-    data = datasets.load_iris()
+    iris = datasets.load_iris()
+    X = iris.data
+    Y = iris.target
 
 if __name__=='__main__':
     main()
