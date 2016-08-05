a1a9620d36ff9b077fe4697eab9ec1d42330db73
Antti Juvonen <akajuvonen@users.noreply.github.com>
2016-08-05 11:41:53 +0300

    Randomized training and test data splitting

--- tests/iris_test.py
+++ tests/iris_test.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from sklearn import datasets
+from random import shuffle
 
 # This example loads the IRIS dataset and classifies
 # using our neural network implementation.
@@ -10,6 +11,14 @@ def main():
     iris = datasets.load_iris()
     X = iris.data
     Y = iris.target
+    # Randomize (shuffle) the indexes
+    # Shuffle randomizes idx in place
+    idx = range(len(X))
+    shuffle(idx)
+    # Split the shuffled indexes into half, to training and test
+    # The int conversion is needed in python 3, I think (for odd number of indexes)
+    train_idx = idx[:int(len(idx)/2)]
+    test_idx = idx[int(len(X)/2):]
 
 if __name__=='__main__':
     main()
