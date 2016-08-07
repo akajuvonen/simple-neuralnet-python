7c43b4aa3a596ae22d7061cb6d063f9578a71344
Antti Juvonen <akajuvonen@users.noreply.github.com>
2016-08-07 13:49:35 +0300

    Added rounding results to int

--- tests/iris_test.py
+++ tests/iris_test.py
@@ -29,9 +29,7 @@ def main():
     net = NeuralNet(X[train_idx],Y_bin[train_idx])
     # Classify
     results = net.classify(X[test_idx])
-    print(results)
-    # The current nn implementation seems unable to classify the iris data correctly
-    # It works well with the simple test case in the neuralnet.py file
+    print(np.rint(results))
 
 if __name__=='__main__':
     main()
