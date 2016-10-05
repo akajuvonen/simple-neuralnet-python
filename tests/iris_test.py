a6a247e7872437753cad69f95c5cc5ffefa7d19f
Antti Juvonen <akajuvonen@users.noreply.github.com>
2016-10-05 16:58:52 +0300

    Updated test code styling to PEP8

--- tests/iris_test.py
+++ tests/iris_test.py
@@ -9,6 +9,7 @@ from neuralnet import NeuralNet
 # using our neural network implementation.
 # The results are visualized in a 2D-plot.
 
+
 def main():
     iris = datasets.load_iris()
     X = iris.data
@@ -21,14 +22,14 @@ def main():
     train_idx = idx[:99]
     test_idx = idx[100:]
     # Initialize zero matrix for outputs in binary form
-    Y_bin = np.zeros((len(Y),3),dtype=np.int)
+    Y_bin = np.zeros((len(Y), 3), dtype=np.int)
     # Convert output from int to binary representation for neural network
     for i in range(len(Y)):
         Y_bin[i][Y[i]] = 1
     # Init the neural network
     net = NeuralNet()
     # Train the network
-    net.train(X[train_idx],Y_bin[train_idx])
+    net.train(X[train_idx], Y_bin[train_idx])
     # Classify
     results = net.classify(X[test_idx])
     # Round and convert to int
@@ -37,12 +38,13 @@ def main():
     ok_count = 0
     # Calculate the count
     for i in range(len(results)):
-        if (results[i]==Y_bin[test_idx[i]]).all():
-            ok_count+=1
+        if (results[i] == Y_bin[test_idx[i]]).all():
+            ok_count += 1
     # How many per cent did we get right
     ok_percentage = float(ok_count) / len(results) * 100
     # Print the result
-    print('Correctly classified ' + str(ok_percentage) + '% of the testing points')
+    print('Correctly classified ' + str(ok_percentage) +
+          '% of the testing points')
 
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
--- tests/test_neuralnet.py
+++ tests/test_neuralnet.py
@@ -4,8 +4,10 @@ from neuralnet import NetworkNotTrainedException
 from neuralnet import TrainingNotSuccessfulException
 import numpy as np
 
+
 class NeuralNetTest(unittest.TestCase):
     """Tests for NeuralNet class"""
+
     def setUp(self):
         """Setup method for neural net tests"""
         self.net = NeuralNet(max_iterations=10000)
@@ -13,17 +15,17 @@ class NeuralNetTest(unittest.TestCase):
     def testSigmoid(self):
         """Sigmoid method tests"""
         # Sigmoid function value should be close to 0
-        i=-10
+        i = -10
         result = self.net._sigmoid(i)
-        self.assertTrue(result<0.1)
+        self.assertTrue(result < 0.1)
         # Should be 0.5
-        i=0
+        i = 0
         result = self.net._sigmoid(i)
-        self.assertEquals(result,0.5)
+        self.assertEquals(result, 0.5)
         # Should be close to 1
-        i=10
+        i = 10
         result = self.net._sigmoid(i)
-        self.assertTrue(result>0.9)
+        self.assertTrue(result > 0.9)
 
     def testSigmoidDerivative(self):
         """Sigmoid deritative tests"""
@@ -32,32 +34,37 @@ class NeuralNetTest(unittest.TestCase):
         # This is more useful for neural network implementation.
         #
         # Should be close to 0
-        i=-10
+        i = -10
         result = self.net._sigmoid_deriv(self.net._sigmoid(i))
-        self.assertTrue(result<0.1)
+        self.assertTrue(result < 0.1)
         # Should be 0.25
-        i=0
+        i = 0
         result = self.net._sigmoid_deriv(self.net._sigmoid(i))
-        self.assertEquals(result,0.25)
+        self.assertEquals(result, 0.25)
         # Should be close to 0
-        i=10
+        i = 10
         result = self.net._sigmoid_deriv(self.net._sigmoid(i))
-        self.assertTrue(result<0.1)
+        self.assertTrue(result < 0.1)
 
     def testTrainedFlag(self):
         """Test that the trained-flag works correctly"""
         # Should be false before training
         self.assertFalse(self.net.trained)
         # Train the net
-        self.net.train(np.array([[1,0,0],[0,0,1],[1,0,1],[0,1,1]]),np.array([[1],[0],[1],[0]]))
+        self.net.train(np.array([[1, 0, 0], [0, 0, 1], [1, 0, 1], [
+                       0, 1, 1]]), np.array([[1], [0], [1], [0]]))
         # Now should be True after training
         self.assertTrue(self.net.trained)
 
     def testNotTrainedException(self):
         """Test that NetworkNotTrained exception is correctly raised"""
-        self.assertRaises(NetworkNotTrainedException,self.net.classify,np.array([1]),np.array([1]))
+        self.assertRaises(NetworkNotTrainedException,
+                          self.net.classify, np.array([1]), np.array([1]))
 
     def testTrainingNotSuccessfulException(self):
         """Test that exception is raised when training does not succeed"""
         self.net.max_iterations = 1
-        self.assertRaises(TrainingNotSuccessfulException,self.net.train,np.array([[1,0,0],[0,0,1],[1,0,1],[0,1,1]]),np.array([[1],[0],[1],[0]]))
+        self.assertRaises(TrainingNotSuccessfulException, self.net.train,
+                          np.array([[1, 0, 0], [0, 0, 1],
+                                   [1, 0, 1], [0, 1, 1]]),
+                          np.array([[1], [0], [1], [0]]))
