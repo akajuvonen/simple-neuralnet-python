6b67aa1052a555f0108205568e55dcc6bd6a06c2
Antti Juvonen <akajuvonen@users.noreply.github.com>
2016-10-06 09:26:11 +0300

    Changed sigmoid plotter to PEP8 style

--- tools/sigmoid_plotter.py
+++ tools/sigmoid_plotter.py
@@ -5,21 +5,22 @@ import matplotlib.pyplot as plt
 
 # Plots sigmoid function and it's derivative using matplotlib
 
+
 def main():
     # Create the net that includes sigmoid functions
     net = NeuralNet()
 
     # Populate lists with sigmoid values
     sigmoid_values = []
-    for i in range(-10,11):
+    for i in range(-10, 11):
         sigmoid_values.append(net._sigmoid(i))
     sigmoid_deriv_values = []
     for i in sigmoid_values:
         sigmoid_deriv_values.append(net._sigmoid_deriv(i))
 
     # Plot and set some parameters for plt
-    plt.plot(range(-10,11),sigmoid_values)
-    plt.plot(range(-10,11),sigmoid_deriv_values)
+    plt.plot(range(-10, 11), sigmoid_values)
+    plt.plot(range(-10, 11), sigmoid_deriv_values)
     plt.ylabel('Value')
     plt.title('Sigmoid and its derivative')
     plt.grid(True)
@@ -27,5 +28,5 @@ def main():
     plt.savefig('sigmoid.png')
     plt.show()
 
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
