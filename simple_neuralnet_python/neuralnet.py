456e770dd379eed3614fe3e69e17b647670026fb
Antti Juvonen <akajuvonen@users.noreply.github.com>
2020-06-06 17:09:32 +0900

    Add separate requirements file for tests

--- simple_neuralnet_python/neuralnet.py
+++ simple_neuralnet_python/neuralnet.py
@@ -131,13 +131,10 @@ def main():
     # Test data
     test_in = np.array([[1, 0, 1], [0, 1, 0], [1, 1, 1]])
     # Classify
-    test_out = nn.classify(test_in)
-    # Should print approx. 1 0 1
+    results = nn.classify(test_in)
     print('Test data classification results (should be 1 0 1):')
-    result0 = np.rint(test_out.item(0)).astype(int)
-    result1 = np.rint(test_out.item(1)).astype(int)
-    result2 = np.rint(test_out.item(2)).astype(int)
-    print(f"{result0} {result1} {result2}")
+    for result in results:
+        print(np.rint(result[0]))
 
 
 if __name__ == '__main__':
